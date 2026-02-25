cac57f8f5d5d15ecbaace56c08b973fa324a4a62
rassoul-sudo <maryamrassouldiouf@gmail.com>
2026-02-25 23:03:08 +0000

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,51 @@
+import streamlit as st 
+# ---SIDEBAR---
+with st.sidebar:
+    st.title("TECHNICIENNE SUPERIEUR EN GEOMATIQUE")
+    st.subheader("Maréme Diouf")
+    st.markdown("Coordonnées")
+    st.write(" ")
+    st.markdown("""
+    - 📧:maryamrassouldiouf@gmail.com
+    - 📞:782135337
+    - 🌏:HLM_FASS""")
+# --- INfOS ---
+st.write("Géomaticienne de formation , la collecte d'information  spatiale  et la visualisation de ces données pour une bonne analyse et une  prise de décision pertinente")
+st.header("COMPÉTENCES:")
+st.subheader("CARTOGRAPHIE🗺️:")
+st.markdown("""
+- carographie  appliqué à l'urbanisme 
+- cartographie des flux , des reseau ...""")
+st.write(" ")
+st.subheader("Analyse sig📈:")
+st.markdown("""
+- ARCGIS : gestion de base de données 
+- qgis :Traitement de données""")
+st.write(" ")
+st.subheader("PHOTOGRAMMETRIE📷:")
+st.markdown("""
+- Traitement d'images de drone avec PIX4D et Agisoft
+- MNE, MNS ,MNT""")
+st.write(" ")
+st.subheader("TELEDETECTION🛰️:")
+st.markdown("""
+- Traitements d'images satelitaire
+- Correction géométriques et radiométriques avec ERDASIMAGINE""")
+st.write(" ")
+st.subheader("BUREAUTIQUE📁:")
+st.markdown("""
+- Word
+- Excel
+- Power point""")
+st.header("ETUDE ACADEMIQUES🧑🏿‍🎓:")
+st.markdown("""
+- BTS en Géomatique
+- Certification en Informatique et internet
+- Licence 1 pcsm
+- Baccalaureats série S2""")
+st.write(" ")
+st.header("PROJETS PROFESSIONNELS:")
+st.markdown("""
+- Analyse spatiale:Analyse de l'impact de la vallée fossile du sine dans une dynamique de valorisation verte et bleue;
+- Gestion des risques:La géomatique au service de la lutte contre les inondations à Diourbel;
+- Energies renouvelables:Analyse de l'implantation de centrales photovoltaiques en zone rurale""")
